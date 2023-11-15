a38a5d4dd7f236a79559cbe43f059fefd07da800
Eugene <Kozhun12345@gmail.com>
2023-11-15 13:45:24 +0700

    Cleanup code & optimization.

--- src/main/kotlin/com/kozhun/commitmessagetemplate/action/GenerateCommitMessageAction.kt
+++ src/main/kotlin/com/kozhun/commitmessagetemplate/action/GenerateCommitMessageAction.kt
@@ -1,16 +1,19 @@
 package com.kozhun.commitmessagetemplate.action
 
-import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.CommitMessageI
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.kozhun.commitmessagetemplate.service.formatter.impl.CommitMessagePatternFormatter
 
-class GenerateCommitMessageAction : AnAction() {
+class GenerateCommitMessageAction : DumbAwareAction() {
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.EDT
+    }
+
     override fun actionPerformed(anActionEvent: AnActionEvent) {
-        val formatter = anActionEvent.project
-            ?.let { CommitMessagePatternFormatter.getInstance(it) }
-            ?: return
+        val formatter = CommitMessagePatternFormatter.getInstance(anActionEvent.project!!)
         getCommitMessageInput(anActionEvent)
             ?.setCommitMessage(formatter.getCommitMessageTemplate())
     }
