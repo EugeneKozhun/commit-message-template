0b1030b34607455f9698f0f2c065fc5e06c911d2
Eugene <Kozhun12345@gmail.com>
2023-11-26 16:51:09 +0700

    Changed anchor names.

--- src/main/kotlin/com/kozhun/commitmessagetemplate/service/caret/impl/CaretServiceDefaultImpl.kt
+++ src/main/kotlin/com/kozhun/commitmessagetemplate/service/caret/impl/CaretServiceDefaultImpl.kt
@@ -14,15 +14,15 @@ import com.kozhun.commitmessagetemplate.service.caret.CaretService
 class CaretServiceDefaultImpl : CaretService {
 
     override fun getCaretOffsetByAnchor(message: String): Pair<String, Int> {
-        return when (val caretOffset = message.indexOf(CARET_ANCHOR)) {
+        return when (val caretOffset = message.indexOf(CARET_POSITION_ANCHOR)) {
             NOT_FOUND_INDEX -> message to message.length
-            else -> message.replace(CARET_ANCHOR, "") to caretOffset
+            else -> message.replace(CARET_POSITION_ANCHOR, "") to caretOffset
         }
     }
 
     companion object {
-        private const val CARET_ANCHOR = "\$CARET"
         private const val NOT_FOUND_INDEX = -1
+        const val CARET_POSITION_ANCHOR = "\$CARET_POSITION"
 
         @JvmStatic
         fun getInstance(project: Project): CaretServiceDefaultImpl = project.service()
--- src/test/kotlin/com/kozhun/commitmessagetemplate/service/caret/impl/CaretServiceDefaultImplTest.kt
+++ src/test/kotlin/com/kozhun/commitmessagetemplate/service/caret/impl/CaretServiceDefaultImplTest.kt
@@ -1,5 +1,6 @@
 package com.kozhun.commitmessagetemplate.service.caret.impl
 
+import com.kozhun.commitmessagetemplate.service.caret.impl.CaretServiceDefaultImpl.Companion.CARET_POSITION_ANCHOR
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -23,21 +24,21 @@ class CaretServiceDefaultImplTest {
 
     @Test
     fun `should return pair of message without caret anchor and index of caret anchor when present`() {
-        val message = "Hello \$CARET world"
+        val message = "Hello $CARET_POSITION_ANCHOR world"
         val result = caretService.getCaretOffsetByAnchor(message)
         assertEquals("Hello  world" to 6, result)
     }
 
     @Test
     fun `should return pair of message without caret anchor and its index at start when caret anchor is at the beginning of the message`() {
-        val message = "\$CARET Hello world"
+        val message = "$CARET_POSITION_ANCHOR Hello world"
         val result = caretService.getCaretOffsetByAnchor(message)
         assertEquals(" Hello world" to 0, result)
     }
 
     @Test
     fun `should return pair of message without caret anchor and its index at end when caret anchor is at the end of the message`() {
-        val message = "Hello world \$CARET"
+        val message = "Hello world $CARET_POSITION_ANCHOR"
         val result = caretService.getCaretOffsetByAnchor(message)
         assertEquals("Hello world " to 12, result)
     }
